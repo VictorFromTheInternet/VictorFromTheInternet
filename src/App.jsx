96278dae3449e96eae15ec61cbb772fe95f2c5ca
VictorFromTheInternet <vma0430@gmail.com>
2025-03-29 10:40:11 -0500

    adding some assets from a template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
+      <div className={styles.App}></div>
       <h1>Hello World</h1>
     </>
   );
