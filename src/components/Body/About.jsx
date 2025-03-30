0fdb663ab1e9eb66804646bd9fb9ac941ae225d0
VictorFromTheInternet <vma0430@gmail.com>
2025-03-30 11:28:21 -0500

    navbar link transition and starting body section

--- src/components/Body/About.jsx
+++ src/components/Body/About.jsx
@@ -5,13 +5,15 @@ export const About = () =>{
     return(
         <>
             <section id="about" className={styles.mainSection}>
-                <h2>About Me</h2>
-                <p>
-                    I am a passionate Full Stack Developer with a strong foundation in web development technologies. I enjoy creating dynamic and responsive web applications that provide seamless user experiences.
-                </p>
-                <p>
-                    My goal is to continuously learn and improve my skills while contributing to exciting projects that make a difference.
-                </p>
+                <div className={styles.sectionCard}>
+                    <h2>About Me</h2>
+                    <p>
+                        I am a passionate Full Stack Developer with a strong foundation in web development technologies. I enjoy creating dynamic and responsive web applications that provide seamless user experiences.
+                    </p>
+                    <p>
+                        My goal is to continuously learn and improve my skills while contributing to exciting projects that make a difference.
+                    </p>
+                </div>                
             </section>
         </>
     )
