78e9d50bdede24b7d33b4b8cc12a206e1b8ad174
VictorFromTheInternet <vma0430@gmail.com>
2025-03-31 18:11:32 -0500

    project card links

--- src/components/Body/Projects.jsx
+++ src/components/Body/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Body.module.css'
+import config from './Body.config.json'
 import {getImageUrl} from '../../utils.js'
 
 export const Projects = () =>{
@@ -10,20 +11,29 @@ export const Projects = () =>{
                     <h2 className={styles.xl}>Projects</h2>
                     <div className={styles.projectContainer}>
                         
-                        <div className={styles.projectCard}>                            
-                            <img src={getImageUrl('projects/project.png')} alt="" />
+                        <div className={styles.projectCard}>        
+                            <a href={config.link_invoice_generator}>
+                                <img src={getImageUrl('projects/project.png')} alt="" />
+                                <h3 className={styles.projectImgLabel}>View Project</h3>
+                            </a>                                                
                             <h4>Invoice Generator</h4>
                             <p>A full stack app to generate a templated invoice based on form data. The tools implemented are: Express, Puppeteer & Nunjucks </p>
                         </div>
-                        <div className={styles.projectCard}>                            
-                            <img src={getImageUrl('projects/project.png')} alt="" />
+                        <div className={styles.projectCard}>
+                            <a href={config.link_json_to_csv}>
+                                <img src={getImageUrl('projects/project.png')} alt="" />
+                                <h3 className={styles.projectImgLabel}>View Project</h3>
+                            </a>                                                        
                             <h4>JSON to CSV API</h4>
                             <p>
                                 A Node.js API for converting JSON into a CSV formatted string, returned as base64 and a data url. 
                             </p>
                         </div>
-                        <div className={styles.projectCard}>                            
-                            <img src={getImageUrl('projects/project.png')} alt="" />
+                        <div className={styles.projectCard}>   
+                            <a href={config.link_color_reducer}>
+                                <img src={getImageUrl('projects/project.png')} alt="" />
+                                <h3 className={styles.projectImgLabel}>View Project</h3>
+                            </a>                                                     
                             <h4>Color Reducer</h4>
                             <p>This project lets the user select colors and create a color-reduced version of the image. It works by creating a new array of rounded pixel values for the output image.</p>
                         </div>
